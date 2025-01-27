19a7394627e4fe4d80f394dcda541e552be40288
Advait Parab <advait0012@proton.me>
2025-01-27 07:14:21 +0000

    write csv

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { parse } = require("csv-parse/sync");
+const Papa = require("papaparse");
 
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
@@ -8,46 +8,29 @@ const argv = yargs(hideBin(process.argv)).argv;
 
 // Get input, output files from cli arguments
 const inputFile = argv.input;
-const outputFile = argv.output;
+const outputFile = argv.clean;
+const reportFile = argv.report;
 
 // Function to read from the csv file
-function readCsv(file) {
+function readCSV(file) {
   const fileContent = fs.readFileSync(file, "utf-8");
-  const records = parse(fileContent, {
-    columns: true,
-    skip_empty_lines: true,
+  const records = Papa.parse(fileContent, {
+    header: true,
   });
-  return records;
+  const headers = records.meta.fields;
+  const body = records.data;
+
+  return { headers, body };
 }
 
-const records = readCsv(inputFile);
-// console.log();
-console.log(Object.keys(records[0]));
-function validateHeaders(validHeaders) {
-  const headers = Object.keys(records[0]);
-
-  if (headers.length !== validHeaders.length) {
-    console.log("Invalid headers");
-    return false;
-  }
-
-  let clean = true;
-  headers.forEach((header) => {
-    if (!validHeaders.includes(header)) {
-      console.log("Invalid header");
-      clean = false;
-      return;
-    }
-  });
-
-  return clean;
+function writeCSV(path, data) {
+  const first = data[0];
+  first["Errors"] = "No such error";
+  data[0] = first;
+  // const newData = data.unshift({ ...first, errors: "This is an error" });
+  const stringify = Papa.unparse(data);
+  console.log(stringify);
+  fs.writeFileSync(path, stringify);
 }
 
-console.log(
-  validateHeaders([
-    "Company Name",
-    "LinkedIn Profile URL",
-    "Employee Size",
-    "Website URL",
-  ])
-);
+writeCSV(outputFile, readCSV(inputFile).body);
