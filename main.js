084085b7925a1cf5ebbfb574c0b165f8216e42f3
Advait Parab <advait0012@proton.me>
2025-01-28 08:03:14 +0000

    add validate funtion and test cases

--- main.js
+++ main.js
@@ -1,35 +1,13 @@
-import { readFileSync, writeFileSync } from "fs";
-import Papa from "papaparse";
-
+// Handling CLI Arguments
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
-
 const argv = yargs(hideBin(process.argv)).argv;
 
-// Get input, output files from cli arguments
+// Reading CLI Arguments
 const inputFile = argv.input;
 const outputFile = argv.clean;
 const reportFile = argv.report;
 
-// Function to read from the csv file
-function readCSV(file) {
-  const fileContent = readFileSync(file, "utf-8");
-  const records = Papa.parse(fileContent, {
-    header: true,
-  });
-  const headers = records.meta.fields;
-  const body = records.data;
-
-  return { headers, body };
+function validateData(data){
+  
 }
-
-function writeCSV(path, data) {
-  const first = data[0];
-  first["Errors"] = "No such error";
-  data[0] = first;
-  const stringify = Papa.unparse(data);
-  console.log(stringify);
-  writeFileSync(path, stringify);
-}
-
-writeCSV(outputFile, readCSV(inputFile).body);
