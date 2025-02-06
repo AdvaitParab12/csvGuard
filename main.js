7a657e097c94ee91ad446feba0cbcba7e2d3bc37
Advait Parab <advait0012@proton.me>
2025-02-06 12:51:51 +0000

    code optimization

--- main.js
+++ main.js
@@ -1,49 +1,28 @@
 #!/usr/bin/env node
-import { intro, outro, log, spinner } from "@clack/prompts";
-import chalk from "chalk";
-import core from "./lib/core.js";
-import prompt from "./lib/prompt.js";
+import { intro, spinner, log, outro } from "@clack/prompts";
 import { readCSV, writeCSV } from "./lib/io.js";
+import chalk from "chalk";
+import prompt from "./lib/prompt.js";
+import core from "./lib/core.js";
 
-const success = chalk.bold.blue;
+const introduction = chalk.bold.green;
+const outroColor = chalk.bold.blue;
 
 async function main() {
-  intro(`CSVGuard start your csv validation`);
-
-  const {
-    input,
-    clean: cleanFile,
-    errors: errorsFile,
-    headers,
-  } = await prompt();
-
-  console.log(headers);
-
-  // const s = spinner();
-
-  log.success(input);
-  log.success(cleanFile);
-  log.success(errorsFile);
-
-  // s.start("Reading the file");
-
-  const csvData = readCSV(input);
-  const [clean, errors] = core(csvData.body, headers);
-
-  log.success(success("Validation rules complete"));
-
-  // s.start("Creating new files");
-
-  // Generate clean csv
-  writeCSV(cleanFile, clean);
-  log.success(success("Generated Clean file successfully"));
-
-  // Generate report
-  writeCSV(errorsFile, errors);
-  log.success(success("Generated report successfully!"));
-
-  // s.stop();
-  outro("You're done!");
+  intro(introduction(`CSVGuard validate your LinkedIn leads CSV file.`));
+  const { input, output, report } = await prompt();
+  const { body, headers } = readCSV(input);
+  const { clean, errors } = await core(body, headers);
+  writeCSV(output, clean);
+  if (report) {
+    writeCSV(report, errors);
+  }
+  log.success(
+    introduction(
+      `File validation completed. Check ${output} and ${report} for results.`
+    )
+  );
+  outro(outroColor(`You're all set!`));
 }
 
-await main();
+main();
